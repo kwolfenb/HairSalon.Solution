b6e384bf2b8502f36d6a7a203be7d0ae4a7b9289
Kenny Wolfenberger <kennywolfenberger@gmail.com>
2018-12-07 10:16:57 -0800

    pass GetAll test for Client Model

--- HairSalon.Tests/ModelTests/ClientTests.cs
+++ HairSalon.Tests/ModelTests/ClientTests.cs
@@ -25,5 +25,41 @@ namespace HairSalon.Tests
       Client testClient = new Client("test name", 1, "555-555-5555", "notes");
       Assert.AreEqual(testClient.GetType(), typeof(Client));
     }
+
+    [TestMethod]
+    public void Save_SaveToClientTable_NameMatch()
+    {
+      //Arrange
+      string clientName = "Cathy Client";
+      Client testClient = new Client(clientName, 1, "555-555-5555", "notes");
+
+      //Act
+      testClient.Save();
+      List<Client> clients = Client.GetAll();
+      Client resultClient = clients[0];
+      string resultName = resultClient.GetName();
+      
+      //Assert
+      Assert.AreEqual(clientName, resultName);
+    }
+
+    [TestMethod]
+    public void GetAll_RetrieveAllClientsFromDB_Client()
+    {
+      //Arrange
+      Client clientOne = new Client("Cathy", 1, "555-555-5555", "notes");
+      Client clientTwo = new Client("John", 1, "555-555-5555", "notes");
+      clientOne.Save();
+      clientTwo.Save();
+
+      //Act
+      List<Client> clients = Client.GetAll();
+      Client resultClientOne = clients[0];
+      Client resultClientTwo = clients[1];
+      
+      //Assert
+      Assert.AreEqual(clientOne, resultClientOne);
+      Assert.AreEqual(clientTwo, resultClientTwo);
+    }
   }
 }
--- HairSalon/Models/Client.cs
+++ HairSalon/Models/Client.cs
@@ -21,6 +21,7 @@ namespace HairSalon.Models
             _notes = notes;
         }
         
+//Setters
         public void SetId(int id)
         {
             _id = id;
@@ -63,7 +64,7 @@ namespace HairSalon.Models
             MySqlConnection conn = DB.Connection();
             conn.Open();
             var cmd = conn.CreateCommand() as MySqlCommand;
-            cmd.CommandText = @"SELECT * FROM client WHERE stylist_id = @stylistId;";
+            cmd.CommandText = @"SELECT * FROM clients WHERE stylist_id = @stylistId;";
             cmd.Parameters.AddWithValue("@stylistId", stylistId);
             MySqlDataReader rdr = cmd.ExecuteReader() as MySqlDataReader;
             while(rdr.Read())
@@ -80,9 +81,36 @@ namespace HairSalon.Models
             conn.Close();
             if (conn != null)
             {
-            conn.Dispose();
+                conn.Dispose();
             }
             return clientList;
+        }
+
+        public static List<Client> GetAll()
+        {
+            List<Client> allClients = new List<Client>{};
+            MySqlConnection conn = DB.Connection();
+            conn.Open();
+            var cmd = conn.CreateCommand() as MySqlCommand;
+            cmd.CommandText = @"SELECT * FROM clients;";
+            MySqlDataReader rdr = cmd.ExecuteReader() as MySqlDataReader;
+            while(rdr.Read())
+            {
+                int clientId = rdr.GetInt32(0);
+                string clientName = rdr.GetString(1);
+                int stylist_id = rdr.GetInt32(2);
+                string phone = rdr.GetString(3);
+                string notes = rdr.GetString(4);
+                Client newClient = new Client(clientName, stylist_id, phone, notes);
+                newClient.SetId(clientId);
+                allClients.Add(newClient);
+            }
+            conn.Close();
+            if (conn != null)
+            {
+                conn.Dispose();
+            }
+            return allClients;
         }
 
         public static void ClearAll()
