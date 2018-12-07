cb01adc6bed5a81ddb4bd5f70254da07f4d2ad0b
Kenny Wolfenberger <kennywolfenberger@gmail.com>
2018-12-07 11:48:34 -0800

    Add view for index and stylist index and stylist new pages

--- HairSalon/Controllers/StylistController.cs
+++ HairSalon/Controllers/StylistController.cs
@@ -9,6 +9,23 @@ namespace HairSalon.Controllers
         [HttpGet("/stylist/index")]
         public ActionResult Index()
         {
+            List<Stylist> allStylists = Stylist.GetAll();
+            return View(allStylists);
+        }
+
+        [HttpPost("/stylist/index")]
+        public ActionResult Index(string stylistName, string stylistPhone, string stylistPicture)
+        {
+            Stylist newStylist = new Stylist(stylistName, stylistPhone, stylistPicture);
+            newStylist.Save();
+            List<Stylist> allStylists = Stylist.GetAll();
+            
+            return View("Index", allStylists);
+        }
+
+        [HttpGet("/stylist/new")]
+        public ActionResult New()
+        {
             return View();
         }
     }
